30e999f05e42a2a250265a0f1ec7e053fcc6a553
Anthony Gorski <anthony.developpeurweb@gmail.com>
2026-04-29 11:26:10 +0200

    client side : add navbar component (desktop links + mobile burger menu)

---
+++ front/src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  menuOpen = signal(false);
+
+  constructor(private auth: AuthService) {}
+
+  toggleMenu() {
+    this.menuOpen.update(v => !v);
+  }
+
+  closeMenu() {
+    this.menuOpen.set(false);
+  }
+
+  logout() {
+    this.auth.logout();
+  }
+}
