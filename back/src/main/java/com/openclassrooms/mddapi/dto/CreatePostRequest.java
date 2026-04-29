d7a35edf0d1884ae6386b2a76561f77663244bf6
Anthony Gorski <anthony.developpeurweb@gmail.com>
2026-04-29 12:10:15 +0200

    server side : add CreatePostRequest DTO

---
+++ back/src/main/java/com/openclassrooms/mddapi/dto/CreatePostRequest.java
@@ -0,0 +1,17 @@
+package com.openclassrooms.mddapi.dto;
+
+public class CreatePostRequest {
+
+    private Long topicId;
+    private String title;
+    private String content;
+
+    public Long getTopicId() { return topicId; }
+    public void setTopicId(Long topicId) { this.topicId = topicId; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+}
