2de82350d3859d03b03e846585ea551dcf0f7c73
Anthony Gorski <anthony.developpeurweb@gmail.com>
2026-04-29 12:27:30 +0200

    server side : add UserController (GET /api/users/me, PUT /api/users/me)

---
+++ back/src/main/java/com/openclassrooms/mddapi/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.openclassrooms.mddapi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.openclassrooms.mddapi.dto.UpdateProfileRequest;
+import com.openclassrooms.mddapi.dto.UserDto;
+import com.openclassrooms.mddapi.model.User;
+import com.openclassrooms.mddapi.service.IUserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final IUserService userService;
+
+    public UserController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public UserDto me(@AuthenticationPrincipal User user) {
+        return userService.toDto(user);
+    }
+
+    @PutMapping("/me")
+    public ResponseEntity<?> updateProfile(@AuthenticationPrincipal User user,
+                                           @RequestBody UpdateProfileRequest request) {
+        try {
+            return ResponseEntity.ok(userService.updateProfile(user.getEmail(), request));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
