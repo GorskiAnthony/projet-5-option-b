b7810a997a752eabee65635ae832e38870db8b5d
Anthony Gorski <anthony.developpeurweb@gmail.com>
2026-04-29 11:26:17 +0200

    client side : add feed page with responsive article grid

---
+++ front/src/app/features/feed/feed.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { PostCardComponent } from '../../shared/components/post-card/post-card.component';
+import { PostService } from '../../core/services/post.service';
+import { Post } from '../../shared/models/post.model';
+
+@Component({
+  selector: 'app-feed',
+  standalone: true,
+  imports: [RouterLink, NavbarComponent, PostCardComponent],
+  templateUrl: './feed.component.html',
+  styleUrl: './feed.component.css'
+})
+export class FeedComponent implements OnInit {
+  posts = signal<Post[]>([]);
+  loading = signal(true);
+  sortDesc = signal(true);
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit() {
+    this.postService.getFeed().subscribe({
+      next: posts => {
+        this.posts.set(posts);
+        this.loading.set(false);
+      },
+      error: () => this.loading.set(false)
+    });
+  }
+
+  toggleSort() {
+    this.sortDesc.update(v => !v);
+    this.posts.update(posts =>
+      [...posts].sort((a, b) => {
+        const diff = new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+        return this.sortDesc() ? -diff : diff;
+      })
+    );
+  }
+}
