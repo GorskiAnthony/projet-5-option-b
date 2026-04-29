7398f50dde6b8877296b4c2e1d2f92ee1d89f1cd
Anthony Gorski <anthony.developpeurweb@gmail.com>
2026-04-29 12:09:07 +0200

    server side : add CommentController (GET /api/posts/{id}/comments)

---
+++ back/src/main/java/com/openclassrooms/mddapi/controller/CommentController.java
@@ -0,0 +1,27 @@
+package com.openclassrooms.mddapi.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.openclassrooms.mddapi.dto.CommentDto;
+import com.openclassrooms.mddapi.service.ICommentService;
+
+@RestController
+@RequestMapping("/api/posts/{postId}/comments")
+public class CommentController {
+
+    private final ICommentService commentService;
+
+    public CommentController(ICommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @GetMapping
+    public List<CommentDto> getByPost(@PathVariable Long postId) {
+        return commentService.getByPost(postId);
+    }
+}
