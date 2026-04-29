2fe63adeb6851b01ac174cd70da96a44e04f58d8
Anthony Gorski <anthony.developpeurweb@gmail.com>
2026-04-29 11:41:20 +0200

    client side : add profile page with subscriptions management

---
+++ front/src/app/features/profile/profile.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { UserService } from '../../core/services/user.service';
+import { TopicService } from '../../core/services/topic.service';
+import { Topic } from '../../shared/models/topic.model';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [ReactiveFormsModule, NavbarComponent],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent implements OnInit {
+  private fb = inject(FormBuilder);
+  private userService = inject(UserService);
+  private topicService = inject(TopicService);
+
+  subscriptions = signal<Topic[]>([]);
+  saved = signal(false);
+  error = signal('');
+
+  form = this.fb.group({
+    username: [''],
+    email: [''],
+    password: ['']
+  });
+
+  ngOnInit() {
+    this.userService.getProfile().subscribe({
+      next: user => this.form.patchValue({ username: user.username, email: user.email })
+    });
+    this.topicService.getAll().subscribe({
+      next: topics => this.subscriptions.set(topics.filter(t => t.subscribed))
+    });
+  }
+
+  onSave() {
+    this.error.set('');
+    const { username, email, password } = this.form.value;
+    const body: Record<string, string> = { username: username!, email: email! };
+    if (password) body['password'] = password;
+
+    this.userService.updateProfile(body).subscribe({
+      next: () => { this.saved.set(true); setTimeout(() => this.saved.set(false), 2000); },
+      error: () => this.error.set('Erreur lors de la sauvegarde.')
+    });
+  }
+
+  unsubscribe(topic: Topic) {
+    this.topicService.unsubscribe(topic.id).subscribe(() =>
+      this.subscriptions.update(list => list.filter(t => t.id !== topic.id))
+    );
+  }
+}
